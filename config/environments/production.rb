2d2f42664664fd9c262dedc8f350aeac1e00f87f
isbaysoft <isbaysoft@gmail.com>
2012-06-18 17:32:19 +0300

    sendmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,9 +36,9 @@ Textbuster::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'http://textbuster.mobilezapp.de' }
-  config.action_mailer.delivery_method = :sendmail
+  # config.action_mailer.delivery_method = :sendmail
 
 
   # Enable threaded mode
