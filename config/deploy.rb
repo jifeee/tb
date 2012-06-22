34399df7e62e298fd2fc5dee86f7955aa1d53d12
isbaysoft <isbaysoft@gmail.com>
2012-06-22 10:33:52 +0300

    test db

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,6 +11,7 @@ task :tst do
   set :password, "c9J0mcCQ1"
   set :keep_releases, 2
 
+  set :rails_env, "test"
   set :scm, "git"
   set :repository, "https://juliavetl:VbifRheu10@github.com/mobilezapp/Access2.git"
   set :branch, "master"
